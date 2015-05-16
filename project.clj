3a8b4f6a607be9224644b50adea1d2cfd6de1cf7
Albin Stjerna <albin.stjerna@gmail.com>
2015-05-16 21:31:45 +0200

    Some more files needing cleaning up.

--- project.clj
+++ project.clj
@@ -99,4 +99,4 @@
 
     ;; Make Lein clean clean up the compiled JS files, useful to avoid
     ;; problems when switching environs.
-    :clean-targets ^{:protect false} [[:target-path :compile-path] "resources/public/js/"])
+    :clean-targets ^{:protect false} [[:target-path :compile-path] "resources/public/js/" "target/"])
